38b7dc832aa6f1fb0367b70e94ad16e82515ebd2
ChristaPigeon <cstath26@gmail.com>
2021-08-04 11:38:03 -0700

    docs: Fix city capitalization in python comments

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -16,7 +16,7 @@ def get_filters():
         (str) day - name of the day of week to filter by, or "all" to apply no day filter
     """
     print('Hello! Let\'s explore some US bikeshare data!')
-    # get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
+    # get user input for city (Chicago, New York City, Washington). HINT: Use a while loop to handle invalid inputs
     while True:
         city = input('Would you like to see data for Chicago, New York City, or Washington?\n').lower()
         if city == 'chicago':
